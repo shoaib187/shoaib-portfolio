4edfce95c6b9a3e36c3d75c6237192341a83af79
Muhammad Shoaib <shoaibriaze@gmail.com>
2024-10-29 20:19:05 +0500

    ok

--- server/routes/route.js
+++ server/routes/route.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { EmptyPage, GetUsers } = require("../controllers/controller");
 
 const router = express.Router();
-router.route("/").get(EmptyPage);
-router.route("/api/get_users").get(GetUsers);
+router.route("/").get(GetUsers);
+router.route("/api/get_users").get(EmptyPage);
 
 module.exports = router;
